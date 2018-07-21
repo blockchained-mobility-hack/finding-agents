09d7e28e4212f1fcffe35b39f60b15650b79b893
Jared Weinfurtner <jared.weinfurtner@bosch-si.com>
2018-07-21 14:41:49 +0200

    first agent commit

---
+++ car-agent/src/main/java/com/findingagents/caragent/CarAgentApplication.java
@@ -0,0 +1,12 @@
+package com.findingagents.caragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarAgentApplication.class, args);
+    }
+}
