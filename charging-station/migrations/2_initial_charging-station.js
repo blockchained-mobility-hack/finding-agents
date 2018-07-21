6317d953cdd93dc1ec7cffd4d988736cecbf559a
Jared Weinfurtner <jared.weinfurtner@bosch-si.com>
2018-07-21 14:51:08 +0200

    fix for truffle migrating constructor with parameters

--- charging-station/migrations/2_initial_charging-station.js
+++ charging-station/migrations/2_initial_charging-station.js
@@ -1,5 +1,5 @@
 var MyChargingStation = artifacts.require("./MyChargingStation.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MyChargingStation);
+  deployer.deploy(MyChargingStation, "51.0000000,8.0000000", "Type2");
 };
