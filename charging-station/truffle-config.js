8cc22217e9fdc0c1106941a07e7b1f147c04369e
Jared Weinfurtner <jared.weinfurtner@bosch-si.com>
2018-07-22 10:38:47 +0200

    adding configuration to test network

--- charging-station/truffle-config.js
+++ charging-station/truffle-config.js
@@ -1,9 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "review fine deal citizen expand truck mother tobacco carpet crystal promote try";
+
 module.exports = {
     networks: {
         development: {
             host: 'localhost',
             port: 8545,
             network_id: '*'
+        },
+        ddf: {
+            provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/fdd6f6ca0a29462e9e61ec616b8b07d9"),
+            network_id: 4
         }
     }
+
 };
