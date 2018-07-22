8d0f3656c4a2822da3edff5bf836eea60978a0b0
ph-lp <nano.philipp@googlemail.com>
2018-07-22 11:16:41 +0200

    bugfixing

--- charging-station/contracts/ParkingLot.sol
+++ charging-station/contracts/ParkingLot.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 interface ParkingLot {
 
-    function getAvailability external pure returns (string);
+    function getAvailability() external pure returns (string);
 
     function getGeoLocation() external pure returns(string);
     
