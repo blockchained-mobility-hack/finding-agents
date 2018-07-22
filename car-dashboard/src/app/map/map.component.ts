69fdd9015b50156579954594bca4589dacd43696
Jared Weinfurtner <jared.weinfurtner@bosch-si.com>
2018-07-22 13:34:01 +0200

    DINGSBUMS

--- car-dashboard/src/app/map/map.component.ts
+++ car-dashboard/src/app/map/map.component.ts
@@ -15,11 +15,18 @@ export class MapComponent implements OnInit {
       y: number;
     }}> = [];
 
+  public parkingLots: Array<{
+    geolocation: {
+      x: number;
+      y: number;
+    }}> = [];
+
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
 
+    // charging stations
     this.http
       .get("http://localhost:8080/api/chargingStationModels", {
         headers: new HttpHeaders({
@@ -45,6 +52,37 @@ export class MapComponent implements OnInit {
             geolocation: {
               x: Number(locations._embedded.chargingStationModels[i].longitude.toFixed(4)),
               y: Number(locations._embedded.chargingStationModels[i].latitude.toFixed(4))
+            }
+          });
+        }
+
+      });
+
+    // parking lots
+    this.http
+      .get("http://localhost:8080/api/parkingLotModels", {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Authorization': 'my-auth-token'
+        })
+      })
+      .subscribe((locations: {
+        _embedded: {
+          parkingLotModels: [
+            {
+              plugType: string,
+              latitude: number,
+              longitude: number
+            }
+            ]
+        }
+      }) => {
+
+        for (let i = 0; i < locations._embedded.parkingLotModels.length; i++) {
+          this.parkingLots.push({
+            geolocation: {
+              x: Number(locations._embedded.parkingLotModels[i].longitude.toFixed(4)),
+              y: Number(locations._embedded.parkingLotModels[i].latitude.toFixed(4))
             }
           });
         }
